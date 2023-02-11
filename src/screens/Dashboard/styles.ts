9cbf6b111768f7a5220cf9bd3b597cba334086fb
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-02-10 21:55:01 -0300

    Add Component and Flatlist

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -75,3 +75,19 @@ export const HighlightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+
+  margin-bottom: 16px;
+`;
+
+export const TransactionList = styled.FlatList``;
