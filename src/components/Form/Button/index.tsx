d2f432db73747c7edffe94279fac97c087f3985b
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-02-13 19:24:12 -0300

    Create Button

---
+++ src/components/Form/Button/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Title } from "./styles";
+
+import { TouchableOpacityProps } from "react-native";
+
+type ButtonProps = TouchableOpacityProps & {
+  title: string;
+};
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <Container {...rest}>
+      <Title>{title}</Title>
+    </Container>
+  );
+}
---
+++ src/components/Form/Button/styles.ts
@@ -0,0 +1,16 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.TouchableOpacity`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  align-items: center;
+
+  padding: 18px;
+  border-radius: 5px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+`;
